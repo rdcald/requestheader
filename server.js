70adc0cc23d599f10e0fbc9ddd0daddcc897095c
ronnie <rdcaldwell5705@eagle.fgcu.edu>
2016-06-20 19:58:00 +0000

    App

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var userAgent = require("useragent");
+var userAgent = require("ua-parser-js");
 var app = express();
 var port = process.env.PORT;
 
@@ -9,10 +9,15 @@ app.listen(port || 8080, process.env.IP || "0.0.0.0", function(){
 
 app.get("/", function(req, res){
     var agent = userAgent.parse(req.headers['user-agent']);
-    var operatingSystem = agent.os.toString();
-    agent.device.toJSON(); // returns an object
-    res.send(JSON.stringify(agent.device));
+    
+    var operatingSystem = agent.getOS().toString();
+    var device = agent.device.vendor.toString();
+    var browser = agent.browser.name.toString();
+    
     res.json({
+        "browser": browser,
+        "device": device,
         "software": operatingSystem  
     });
+    
 })
