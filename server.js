a464effc55ae3b5e4a19a32f04f97de9d8b22fa9
ronnie <rdcaldwell5705@eagle.fgcu.edu>
2016-06-20 20:28:55 +0000

    App

--- server.js
+++ server.js
@@ -9,15 +9,26 @@ app.listen(port || 8080, process.env.IP || "0.0.0.0", function(){
 
 app.get("/", function(req, res){
     var agent = parser(req.headers['user-agent']);
+    
     var osName = agent.os.name;
     var osVersion = agent.os.version;
     var operatingSystem = osName + " " + osVersion;
+    
     var browser = agent.browser.name;
+    var device = agent.device.type;
+    
+    var vendor = agent.device.vendor;
+    var cpu = agent.cpu.architecture;
+    var system = vendor + " " + cpu;
     
     JSON.stringify(operatingSystem);
     JSON.stringify(browser);
+    JSON.stringify(device);
+    JSON.stringify(system);
     
     res.json({
+        "device": device,
+        "system": system,
         "browser": browser,
         "software": operatingSystem
     });
