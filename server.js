9e96d623adc65f5f72a77f996f96c62de560e3b6
ronnie <rdcaldwell5705@eagle.fgcu.edu>
2016-06-20 19:26:57 +0000

    App

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var userAgent = require("useragent");
 var app = express();
+var port = process.env.PORT;
 
-app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-    var addr = app.address();
-    console.log("Chat server listening at", addr.address + ":" + addr.port);
+app.listen(port || 8080, process.env.IP || "0.0.0.0", function(){
+    console.log("Header parser server listening at:" + port);
 });
 
 app.get("/", function(req, res){
